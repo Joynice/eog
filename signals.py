0e6e9a1b25c7a91ac76824627843f9eeb54734dd
Joynice <1125365907@qq.com>
2018-11-12 14:26:03 +0800

    修改signals

--- signals.py
+++ signals.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # -*- coding: UTF-8 -*-
 __author__ = 'Joynice'
 from blinker import Namespace
@@ -39,33 +38,4 @@ def change_username(sender, operate_time,ip,operate_datail):
 login_signal.connect(login_log)
 logout_signal.connect(logout_log)
 account_signal.connect(change_lcon)
-=======
-# -*- coding: UTF-8 -*-
-__author__ = 'Joynice'
-from blinker import Namespace
-from front.models import Log
-from flask import g
-import datetime
 
-
-namespace = Namespace()
-
-login_signal = namespace.signal('login')
-logout_signal = namespace.signal('logout')
-
-
-def login_log(sender, handler, ip, login_time):
-    log = Log(handler=handler, login_time=login_time, ip=ip, logout_time=(datetime.datetime.now()+datetime.timedelta(hours=3)).strftime("%Y-%m-%d %H:%M:%S"))
-    log.save()
-
-
-def logout_log(sender, logout_time):
-    print(g.eog_user.email)
-    print(g.user_log.ip)
-    g.user_log.logout_time = logout_time
-    g.user_log.save()
-
-
-login_signal.connect(login_log)
-logout_signal.connect(logout_log)
->>>>>>> 6e9c628b092a4d60e546850fc6b0979f66f8ae3a
